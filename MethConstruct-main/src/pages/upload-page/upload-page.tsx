4e560e7f08ec63909123f8967420b1b72409c198
Kirillkuk <khryavin@list.ru>
2023-07-06 19:30:54 +0300

    add name, profile,year on upload-page

--- MethConstruct-main/src/pages/upload-page/upload-page.tsx
+++ MethConstruct-main/src/pages/upload-page/upload-page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Input from "@mui/material/Input";
